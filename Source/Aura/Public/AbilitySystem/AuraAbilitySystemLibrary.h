441270af49ced93630541c494861ddd67a4fc11a
JunHyung777 <jameskjh7531@gmail.com>
2024-07-22 07:57:44 +0900

    Aura Ability System Blueprint Library

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,19 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
